5a1ea231ff07ba7b9a15d947a9482ca264507792
Jakub Kozák <Jakub@Jakub-2.local>
2015-03-15 23:13:38 +0100

    - add user

---
+++ app/presenters/UsereditPresenter.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette,
+	App\Model;
+
+
+/**
+ * User Edit presenters.
+ */
+class UsereditPresenter extends BasePresenter
+{
+
+	    /** @var \App\Model\Users @inject */
+    public $users;
+
+	public function renderDefault() {
+		
+	}
+
+	/**
+	 * Add User form factory.
+	 * @return Nette\Application\UI\Form
+	 */
+	protected function createComponentAddUserForm()
+	{
+		$form = new Nette\Application\UI\Form;
+		$form->addText('jmeno', 'Jméno:')
+			->setRequired('Please enter your username.');
+		
+		$form->addText('login', 'Login:')
+			->setRequired('Please enter your login.');
+
+		$form->addPassword('heslo', 'Heslo:')
+			->setRequired('Please enter your password.');
+
+		$form->addSubmit('send', 'Uložit');
+
+		// call method signInFormSucceeded() on success
+		$form->onSuccess[] = array($this, 'addUserFormSucceeded');
+		return $form;
+	}
+
+
+	public function addUserFormSucceeded($form, $values)
+	{
+		//$UsersModel = $this->context->Users;
+		
+		try {
+			$this->users->addUser($values);
+			$this->redirect('Homepage:');
+            //$this->redirect('Admin:default');
+
+		} catch (\Nette\Neon\Exception $e) {
+			$form->addError($e->getMessage());
+		}
+	}
+
+
+}
